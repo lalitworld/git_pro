b3969701f1fccaca24a9c637dd65138a0f366ab5
Hitesh Chaudhari <lalit.chaudhari0@gmail.com>
2022-08-30 16:20:16 +0530

    teacher models added

--- gitapp/models.py
+++ gitapp/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    number = models.IntegerField()
+    class Meta:
+        db_name = 'Teacher'
+    
